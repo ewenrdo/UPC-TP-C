d304a9fbe8f20ee0ae763b04ec65df69dba48009
Ewen Rodrigues de Oliveira <rodriguesdee@y13.script.univ-paris-diderot.fr>
2026-02-10 12:26:33 +0100

    exo4

--- TP3/exo4.c
+++ TP3/exo4.c
@@ -2,19 +2,55 @@
 #include "graphiques.h"
 #include <math.h>
 
-int
-main()
-{
-    gr_init(800, 400);
-    
-    for (int x = 0; x < 800; x++)
-    {
-        gr_draw_point(x, 200 + 200*cos((x * M_PI * 2) / 800), 0,0,0);
-    }
-    
+unsigned syracuse(int m);
 
-    gr_display();
+int main()
+{
+    gr_init(800, 400);  
+    
+    unsigned max_steps = 0; 
+
+    // Pour mettre à l'échelle
+    for (int n = 1; n <= 200; n++) {
+        unsigned steps = syracuse(n);
+        if (steps > max_steps) {
+            max_steps = steps;  // Trouver le nombre maximum d'étapes
+        }
+    }
+
+    for (int n = 1; n <= 200; n++) {
+        unsigned steps = syracuse(n);
+
+        // Normalisation de y pour qu'il reste dans l'intervalle [0, 400]
+        int y = (int)((float)steps / max_steps * 400);
+
+        gr_draw_point(n, 400 - y, 0, 0, 0);  // 400 - y pour que les petites valeurs soient en bas
+    }
+
+    gr_display();  
     return 0;
+}
+
+// Fonction pour calculer le nombre d'étapes de la séquence de Syracuse pour un nombre donné
+unsigned syracuse(int m)
+{
+    int res = m;
+    unsigned flight_time = 0;
+
+    while (res != 1)  // La séquence s'arrête lorsqu'on atteint 1
+    {
+        flight_time++;  // Incrémentation du nombre d'étapes
+        if (res % 2 == 0)
+        {
+            res = res / 2;  // Si m est pair, on divise par 2
+        }
+        else
+        {
+            res = 3 * res + 1;  // Si m est impair, on applique la formule 3m + 1
+        }
+    }
+
+    return flight_time;  // Retourne le nombre d'étapes avant d'atteindre 1
 }
 
 unsigned syracuse(int m)
